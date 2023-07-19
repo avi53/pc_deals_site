6c9197f8ff705f982fc051a4c03bee0643195988
Sam <74118063+Reid910@users.noreply.github.com>
2023-07-18 21:06:57 -0700

    js

--- Javascript/hello.js
+++ Javascript/hello.js
@@ -1,6 +1,16 @@
 console.log("W-balls");
 
-for (let i = 1; i <= 150; i++) {
-    let tik = "-"
-    console.log(`${tik.repeat(i)} PC DEALS ${tik.repeat(i)}`);
-}
+// for (let i = 1; i <= 150; i++) {
+//     let tik = "-"
+//     console.log(`${tik.repeat(i)} PC DEALS ${tik.repeat(i)}`);
+// }
+
+// let image = document.querySelector("PC").firstChild;
+
+// let apiCall = null
+
+// image.innerHtml = ` src = ${apiCall}`
+
+// console.log()
+
+// api cost monez :C
